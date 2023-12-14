5027a2cee48112caf27bf83fe2098cc03816ce36
BlenDMinh <53397490+BlenDMinh@users.noreply.github.com>
2023-12-14 16:43:34 +0700

    .

--- cv/src/main/java/controller/AuthenticationServlet.java
+++ cv/src/main/java/controller/AuthenticationServlet.java
@@ -13,8 +13,6 @@ import model.bo.AuthenticationBO;
 public class AuthenticationServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getOutputStream().print(
-                AuthenticationBO.getInstance().login("themysmine@gmail.com", "ComTMM0112"));
-
+        req.getRequestDispatcher("login.html").forward(req, resp);
     }
 }
---
+++ cv/src/main/java/controller/RegisterServlet.java
@@ -0,0 +1,9 @@
+package controller;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet(urlPatterns = "/register")
+public class RegisterServlet extends HttpServlet {
+    
+}

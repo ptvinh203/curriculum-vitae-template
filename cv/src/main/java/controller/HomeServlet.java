15445b07cd70bcda4481c1b891ca9525ceea3534
Phạm Thành Vinh <thanhvinh07032003@gmail.com>
2023-12-15 11:00:27 +0700

    HomeServlet

---
+++ cv/src/main/java/controller/HomeServlet.java
@@ -0,0 +1,18 @@
+package controller;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/home")
+public class HomeServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("home.html").forward(req, resp);
+    }
+}
